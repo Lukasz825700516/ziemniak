630035701506e1c2435c410d6be1317858d141fb
Łukasz <lukasz@lukaszm.xyz>
2021-01-02 21:26:30 +0100

    Added imgui with docking

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,71 @@
-int main() {
+#include <iostream>
+#include <cstdlib>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+int main() {
+	GLFWwindow* window;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(800, 600, "ziemniak", 0, 0);
+	if (!window) {
+		std::cerr << "Failed to create window!" << std::endl;
+		std::exit(1);
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		std::exit(1);
+	} 
+	glad_glViewport(0, 0, 800, 600);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330 core");
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glad_glClearColor(0.1, 0.5, 0.5, 0.1);
+		glad_glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::DockSpaceOverViewport();
+
+		ImGui::Begin("Hello world!");
+		ImGui::End();
+
+		ImGui::Begin("HELLLLOOOO world!");
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
